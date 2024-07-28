b065c690fddc22b74f3485f8ce1fcb2dc35ee0bf
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-07-28 19:48:26 +0300

    add spinner to details

--- client/src/components/main/details/Details.jsx
+++ client/src/components/main/details/Details.jsx
@@ -1,73 +1,83 @@
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 import styles from './Details.module.css';
+import Spinner from '../spinner/Spinner';
 
 import * as tradeAPI from '../../../api/trades-api';
-import { useParams } from 'react-router-dom';
 
 export default function Details() {
     const [trade, setTrade] = useState({});
-    const { tradeId } = useParams()
+    const [loading, setLoading] = useState(true);
+    const { tradeId } = useParams();
 
     useEffect(() => {
         (async () => {
-            const result = await tradeAPI.getOne(tradeId)
+            const result = await tradeAPI.getOne(tradeId);
             setTrade(result);
+            setLoading(false);
         })();
-    })
+    }, [tradeId]);
 
     return (
-        <div className={styles.detailsContainer}>
-            <section className={styles.detailsCard}>
-                <header className={styles.detailsHeader}>
-                    <h4>Trade Details</h4>
-                </header>
-                <article className={styles.detailsContent}>
-                    <div className={styles.detailsItem}>
-                        <span className={styles.label}>Ticker:</span>
-                        <span className={styles.value}>{trade.ticker}</span>
-                    </div>
-                    <div className={styles.detailsItem}>
-                        <span className={styles.label}>Date:</span>
-                        <span className={styles.value}>{trade.date}</span>
-                    </div>
-                    <div className={styles.detailsItem}>
-                        <span className={styles.label}>Trade Direction:</span>
-                        <span className={styles.value}>{trade.trade_direction}</span>
-                    </div>
-                    <div className={styles.detailsItem}>
-                        <span className={styles.label}>Entry Price:</span>
-                        <span className={styles.value}>{trade.entry}</span>
-                    </div>
-                    <div className={styles.detailsItem}>
-                        <span className={styles.label}>Exit Price:</span>
-                        <span className={styles.value}>{trade.exit}</span>
-                    </div>
-                    <div className={styles.detailsItem}>
-                        <span className={styles.label}>Volume:</span>
-                        <span className={styles.value}>{trade.volume}$</span>
-                    </div>
-                    <div className={styles.detailsItem}>
-                        <span className={styles.label}>Support:</span>
-                        <span className={styles.value}>{trade.support}</span>
-                    </div>
-                    <div className={styles.detailsItem}>
-                        <span className={styles.label}>MA:</span>
-                        <span className={styles.value}>{trade.ma}</span>
-                    </div>
-                    <div className={styles.detailsItem}>
-                        <span className={styles.label}>Price Action:</span>
-                        <span className={styles.value}>{trade.price_action}</span>
-                    </div>
-                    <div className={styles.detailsItem}>
-                        <span className={styles.label}>Oscillators:</span>
-                        <span className={styles.value}>{trade.oscilators}</span>
-                    </div>
-                </article>
-                <footer className={styles.detailsFooter}>
-                    <button className={styles.actionButton}>Edit</button>
-                    <button className={styles.actionButton}>Delete</button>
-                </footer>
-            </section>
-        </div>
+        <>
+            {loading ? (
+                <Spinner />
+            ) : (
+                <div className={styles.detailsContainer}>
+                    <section className={styles.detailsCard}>
+                        <header className={styles.detailsHeader}>
+                            <h4>Trade Details</h4>
+                        </header>
+                        <article className={styles.detailsContent}>
+                            <div className={styles.detailsItem}>
+                                <span className={styles.label}>Ticker:</span>
+                                <span className={styles.value}>{trade.ticker}</span>
+                            </div>
+                            <div className={styles.detailsItem}>
+                                <span className={styles.label}>Date:</span>
+                                <span className={styles.value}>{trade.date}</span>
+                            </div>
+                            <div className={styles.detailsItem}>
+                                <span className={styles.label}>Trade Direction:</span>
+                                <span className={styles.value}>{trade.trade_direction}</span>
+                            </div>
+                            <div className={styles.detailsItem}>
+                                <span className={styles.label}>Entry Price:</span>
+                                <span className={styles.value}>{trade.entry}</span>
+                            </div>
+                            <div className={styles.detailsItem}>
+                                <span className={styles.label}>Exit Price:</span>
+                                <span className={styles.value}>{trade.exit}</span>
+                            </div>
+                            <div className={styles.detailsItem}>
+                                <span className={styles.label}>Volume:</span>
+                                <span className={styles.value}>{trade.volume}$</span>
+                            </div>
+                            <div className={styles.detailsItem}>
+                                <span className={styles.label}>Support:</span>
+                                <span className={styles.value}>{trade.support}</span>
+                            </div>
+                            <div className={styles.detailsItem}>
+                                <span className={styles.label}>MA:</span>
+                                <span className={styles.value}>{trade.ma}</span>
+                            </div>
+                            <div className={styles.detailsItem}>
+                                <span className={styles.label}>Price Action:</span>
+                                <span className={styles.value}>{trade.price_action}</span>
+                            </div>
+                            <div className={styles.detailsItem}>
+                                <span className={styles.label}>Oscillators:</span>
+                                <span className={styles.value}>{trade.oscilators}</span>
+                            </div>
+                        </article>
+                        <footer className={styles.detailsFooter}>
+                            <button className={styles.actionButton}>Edit</button>
+                            <button className={styles.actionButton}>Delete</button>
+                        </footer>
+                    </section>
+                </div>
+            )}
+        </>
     );
 }
