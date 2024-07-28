f4c9660b265d56c19b4bd93aa978dda7b787f5da
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-07-28 17:02:11 +0300

    add Spinner

--- client/src/components/main/catalog/Catalog.jsx
+++ client/src/components/main/catalog/Catalog.jsx
@@ -3,19 +3,28 @@ import * as tradesAPI from '../../../api/trades-api';
 
 import Card from "../card/Card";
 import styles from './Catalog.module.css';
+import Spinner from '../spinner/Spinner';
 
 export default function Catalog() {
     const [trades, setTrades] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         tradesAPI.getAll()
-            .then(result => setTrades(result));
-            
+            .then(result => {
+                setTrades(result);
+                setLoading(false);
+            });
     }, []);
 
     return (
         <div className={styles["flex-container"]}>
-            {trades.map(trade => <Card key={trade._id} {...trade}/>)}
+            {loading 
+                ? <Spinner />
+                : trades.length > 0 
+                    ? trades.map(trade => <Card key={trade._id} {...trade}/>)
+                    : <h3>No recorded trades</h3>
+            }         
         </div>
     );
 }
