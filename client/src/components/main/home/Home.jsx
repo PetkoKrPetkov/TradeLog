6acca6122957870d076a92f1e82839b123e7efe8
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-07-29 17:22:29 +0300

    add latest with reverse().slice(0, 3) to be fixed

--- client/src/components/main/home/Home.jsx
+++ client/src/components/main/home/Home.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Card from "../card/Card";
 import styles from './Home.module.css';
+import * as tradesAPI from '../../../api/trades-api';
+import Spinner from '../spinner/Spinner';
 
 export default function Home() {
+    const [latestTrades, setLatestTrades] = useState([])
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const result = await tradesAPI.getAll();
+                setLatestTrades(result.reverse().slice(0 , 3));
+            } catch (error) {
+                console.error('Error fetching trades:', error);
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, []);
+
     return (
         <div className={styles["flex-container"]}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {loading 
+                ? <Spinner />
+                : latestTrades.length > 0 
+                    ? latestTrades.map(trade => <Card key={trade._id} {...trade}/>)
+                    : <h3>No recorded trades</h3>
+            }         
         </div>
     );
 }
