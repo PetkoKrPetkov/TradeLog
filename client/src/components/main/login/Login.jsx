ffd94c8bf0f43e3992121055afc734050efb1581
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-07-31 15:26:22 +0300

    rafactoring in login

--- client/src/components/main/login/Login.jsx
+++ client/src/components/main/login/Login.jsx
@@ -2,22 +2,23 @@ import { useNavigate } from 'react-router-dom';
 import { useLogin } from '../../../hooks/useAuth';
 import { useForm } from '../../../hooks/useForm';
 import styles from '../register/Register.module.css';
+const initialValues = { email: '', password: '' }
 
 export default function Login() {
     const login = useLogin();
     const navigate = useNavigate();
-    const { values, changeHandler, submitHandler } = useForm({
-        email: '', password: ''
-    },
-        async ({ email, password }) => { 
-            try {
-                await login(email, password)
-                navigate('/');
-            } catch (error) {
-                console.error(error.message);
-            }
+
+    const loginHandler = async ({ email, password }) => { 
+        try {
+            await login(email, password)
+            navigate('/');
+        } catch (error) {
+            console.error(error.message);
         }
-                
+    }
+    const { values, changeHandler, submitHandler } = useForm(
+        initialValues, 
+        loginHandler,
     );
 
     return (
