bcb03a6f3b3f6f0c8b8a03a9ce70afc5f8bf9641
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-07-31 16:00:14 +0300

    add global authState and context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
+
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Main from './components/main/Main';
+import { AuthContext } from './contexts/AuthContext';
 
 function App() {
+  const [authState, setAuthState] = useState({});
+
+  const changeAuthState = (state) => {
+    setAuthState(state);
+  };
+
+  const contextData = {
+    email: authState.email,
+    accessToken: authState.accessToken,
+    isAuthenticated: !!authState.email,
+    changeAuthState,
+  }
+
   return (
-    <div className='site'>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <AuthContext.Provider value={contextData}>
+      <div className='site'>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </AuthContext.Provider>
   );
 }
 
