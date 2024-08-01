00f2c10374889a105701020cdaeec887ec903bdf
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-08-01 18:10:32 +0300

    add accessToken to requester and add createTrade

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ function App() {
   const [authState, setAuthState] = useState({});
 
   const changeAuthState = (state) => {
+    localStorage.setItem('accessToken', state.accessToken);
+
     setAuthState(state);
   };
 
