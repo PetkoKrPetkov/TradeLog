9a140552c0aa219d116efb1859c5189c747a6949
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-08-03 10:45:05 +0300

    extract AuthContext.Provider in separate context file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,20 @@
-import { useState } from 'react';
 
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Main from './components/main/Main';
-import { AuthContext } from './contexts/AuthContext';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
-  const [authState, setAuthState] = useState({});
-
-  const changeAuthState = (state) => {
-    localStorage.setItem('accessToken', state.accessToken);
-
-    setAuthState(state);
-  };
-
-  const contextData = {
-    userId: authState._id,
-    email: authState.email,
-    accessToken: authState.accessToken,
-    isAuthenticated: !!authState.email,
-    changeAuthState,
-  }
 
   return (
-    <AuthContext.Provider value={contextData}>
+    <AuthContextProvider>
       <div className='site'>
         <Header />
         <Main />
         <Footer />
       </div>
-    </AuthContext.Provider>
+    </AuthContextProvider>
   );
 }
 
