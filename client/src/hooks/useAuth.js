a8a17b0d28d8eab337289672986e0f40c4e0dd41
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-08-03 11:52:45 +0300

    add persisted auth state

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -17,7 +17,7 @@ export const useLogin = () => {
 };
 
 export const useRegister = () => {
-    const { changeAuthState} = useContext(AuthContext);
+    const { changeAuthState} = useAuthContext();
 
     const registerHandler = async (email, password, username) => {
         const result = await register(email, password, username);
