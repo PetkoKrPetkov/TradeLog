8dd35aabdc185dd3758c710b18f1116a7ff157af
PetkoKrPetkov <136084693+PetkoKrPetkov@users.noreply.github.com>
2024-08-03 14:14:24 +0300

    add logout functionality

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -8,21 +8,24 @@ import Profile from './profile/Profile'
 import CreateTrade from './create-trade/CreateTrade'
 import Details from './details/Details'
 import PageNotFound from './page-not-found/PageNotFound'
+import Logout from './logout/Logout'
+
 
 
 export default function Main() {
-    return(
-        
-            <Routes>
-                <Route path='/' element={<Home></Home>}></Route>
-                <Route path='/register' element={<Register></Register>}></Route>
-                <Route path='/login' element={<Login></Login>}></Route>
-                <Route path='/trades' element={<Trades></Trades>}></Route>
-                <Route path='/trades/:tradeId/details' element={<Details></Details>}></Route>              
-                <Route path='/profile' element={<Profile></Profile>}></Route>
-                <Route path='/create-trade' element={<CreateTrade></CreateTrade>}></Route>
-                <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
-            </Routes>         
-        
+    return (
+
+        <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/register' element={<Register></Register>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
+            <Route path='/logout' element={<Logout></Logout>}></Route>
+            <Route path='/trades' element={<Trades></Trades>}></Route>
+            <Route path='/trades/:tradeId/details' element={<Details></Details>}></Route>
+            <Route path='/profile' element={<Profile></Profile>}></Route>
+            <Route path='/create-trade' element={<CreateTrade></CreateTrade>}></Route>
+            <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
+        </Routes>
+
     )
 }
